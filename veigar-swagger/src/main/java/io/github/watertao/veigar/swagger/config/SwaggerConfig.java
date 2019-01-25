979a101dc778d3359ef8e0f8ed86cf13f146dd28
jwatertao@gmail.com <1059912278@qq.com>
2019-01-25 09:17:36 +0800

    增加了 veigar-swagger 组件，并更新了文档中相关的部分

---
+++ veigar-swagger/src/main/java/io/github/watertao/veigar/swagger/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package io.github.watertao.veigar.swagger.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
+public class SwaggerConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(env.getProperty("swagger.api.basePackage")))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(env.getProperty("swagger.api.title"))
+                .version(env.getProperty("swagger.api.version"))
+                .build();
+    }
+
+}
