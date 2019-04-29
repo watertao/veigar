1cfb6eeff5fa084893b6111c395c113057458151
jwatertao@gmail.com <1059912278@qq.com>
2019-04-29 13:52:24 +0800

    [ #4 ] 为 AuthenticationFilter 和 CredentialUpdateFilter 增加了审计日志的逻辑

--- veigar-auth/src/main/java/io/github/watertao/veigar/auth/filter/CredentialUpdateFilter.java
+++ veigar-auth/src/main/java/io/github/watertao/veigar/auth/filter/CredentialUpdateFilter.java
@@ -1,8 +1,12 @@
 package io.github.watertao.veigar.auth.filter;
 
+import io.github.watertao.veigar.core.aspect.RequestPostProcessor;
 import io.github.watertao.veigar.core.exception.UnauthenticatedException;
 import io.github.watertao.veigar.core.filter.AbstractJsonRequestAwareFilter;
 import io.github.watertao.veigar.core.message.LocaleMessage;
+import io.github.watertao.veigar.core.util.HttpRequestHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import io.github.watertao.veigar.core.filter.FilterOrders;
@@ -11,14 +15,20 @@ import io.github.watertao.veigar.session.spi.AuthenticationObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by watertao on 3/26/16.
  */
 public abstract class CredentialUpdateFilter extends AbstractJsonRequestAwareFilter {
 
+  private static final Logger logger = LoggerFactory.getLogger(CredentialUpdateFilter.class);
+
   private static final String VERB = "PUT";
   private static final String URI = "/system/session/credential";
+
+  @Autowired(required = false)
+  private List<RequestPostProcessor> requestPostProcessors;
 
   @Autowired
   private LocaleMessage localeMessage;
@@ -34,6 +44,8 @@ public abstract class CredentialUpdateFilter extends AbstractJsonRequestAwareFil
   @Override
   protected Object handleJson(HttpServletRequest request, HttpServletResponse response, Object json) {
 
+    Long startTime = System.currentTimeMillis();
+
     AuthenticationObject authObj = AuthObjHolder.getAuthObj(request);
 
     if (authObj == null) {
@@ -41,6 +53,24 @@ public abstract class CredentialUpdateFilter extends AbstractJsonRequestAwareFil
     }
 
     updateCredential(authObj, json);
+
+    if (requestPostProcessors != null && requestPostProcessors.size() > 0) {
+      for (RequestPostProcessor processor : requestPostProcessors) {
+        try {
+          logger.info("[ post process ] {}", processor.getClass().getSimpleName());
+          processor.process(
+            HttpRequestHelper.getCurrentRequest(),
+            HttpRequestHelper.getCurrentResponse(),
+            json,
+            authObj,
+            null,
+            System.currentTimeMillis() - startTime
+          );
+        } catch (Throwable e) {
+          logger.error("Error on post process", e);
+        }
+      }
+    }
 
     response.setStatus(HttpStatus.OK.value());
 
